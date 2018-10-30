8054b19656f3e2c06c54453365832e148ca76560
Bimal Kumar <40321@hexaware.com>
2018-10-30 16:59:21 +0530

    sdsd

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path =  require('path');
+var path = require('path');
 var actionsdkApp = require('./dialogflowHandlers');
 var app = express();
 var port = process.env.PORT || 8011;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use(actionsdkApp);
+app.post("/gatest", actionsdkApp);
 
-app.listen(port, function () {
-	console.log(`Application started listening ${port}.`);
+app.listen(port, function() {
+    console.log(`Application started listening ${port}.`);
 });
 
 
